49e4f9b8793036baa84d1c348c95205ea8c62f15
vadivel <gct.vadivel@gmail.com>
2015-11-22 10:48:05 -0600

    just checking in

---
+++ ScreenRender/App_Code/URL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Summary description for URL
+/// </summary>
+public class URL
+{
+    [JsonProperty("u")]
+    public string url { get; set; }
+
+    [JsonProperty("e")]
+    public DateTime expiration { get; set; }    
+}
+
+public class URLCollection
+{
+    [JsonProperty("Data")]
+    public List<URL> urls { get; set; }
+}
